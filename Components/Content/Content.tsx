32ddcf8f7980b8f894b76c0e00124a2079f3fc64
Aditya Rajguru <77228474+Rajguru-Aditya@users.noreply.github.com>
2023-06-26 21:46:46 +0530

    Add dummy products

--- Components/Content/Content.tsx
+++ Components/Content/Content.tsx
@@ -1,11 +1,79 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import styles from './styles'
+import FontistoIcons from 'react-native-vector-icons/Fontisto'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 const Content = () => {
+
+  const dummyData = [
+    {
+      id: 41,
+      name: "nykaa",
+      image: require("../../Assets/Images/product.png"),
+      description: "nyx professional makeup",
+    },
+    {
+      id: 98,
+      name: "nykaa",
+      image: require("../../Assets/Images/product.png"),
+      description: "nyx professional makeup",
+    },
+    {
+      id: 100,
+      name: "nykaa",
+      image: require("../../Assets/Images/product.png"),
+      description: "nyx professional makeup",
+    },
+    {
+      id: 79,
+      name: "nykaa",
+      image: require("../../Assets/Images/product.png"),
+      description: "nyx professional makeup",
+    },
+  ]
+
+  const ProductContainer = ({ product }: any) => (
+    <TouchableOpacity style={styles.productContainer} activeOpacity={0.8}>
+      <View style={styles.productMatchContainer}>
+        <Image source={{uri: "https://img.icons8.com/offices/30/lightning-bolt.png"}} style={styles.matchIcon} />
+        <Text style={styles.productMatchText}>super Match</Text>
+      </View>
+      <View style={styles.productTopContainer}>
+        <Image source={product.image} style={styles.productImage} />
+      </View>
+      <View style={styles.productBottomContainer}>
+        <View style={styles.productBottomTextContainer}>
+          <Text style={styles.name}>{product.name}</Text>
+          <Text style={styles.description}>{product.description}</Text>
+        </View>
+        <View style={styles.productBottomButtonsContainer}>
+          <TouchableOpacity style={styles.productBottomButton} activeOpacity={0.8}>
+            <Text style={styles.productBottomButtonText}>view</Text>
+          </TouchableOpacity>
+            <FontistoIcons name="heart-alt" size={20} color="#000" style={styles.heart}/>
+          <TouchableOpacity style={styles.productBottomOptionsButton} activeOpacity={0.8}>
+            <MaterialCommunityIcons name="dots-horizontal" size={15} color="#000" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
+
   return (
     <ScrollView style={styles.container}>
-      <Text style={styles.text}>Content</Text>
+      <View style={styles.topTextContainer}>
+        <Text style={styles.text}>weekly top 4</Text>
+        <Text style={styles.textSmall}>Perfect-for-you based on your goals!</Text>
+      </View>
+      <View style={styles.productsContainer}>
+        {
+          dummyData.map((product) => (
+              <ProductContainer product={product} key={product.id} />
+            )
+          )
+        }
+      </View>
     </ScrollView>
   )
 }
--- Components/Content/styles.tsx
+++ Components/Content/styles.tsx
@@ -6,10 +6,138 @@ const styles = StyleSheet.create({
     gap: 20,
   },
 
+  topTextContainer: {
+    gap: 5,
+  },
+
   text: {
     fontSize: 20,
     fontWeight: 'bold',
     color: '#000',
+  },
+
+  textSmall: {
+    fontSize: 14,
+    color: '#000',
+    fontWeight: 'bold',
+  },
+
+  productsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 20,
+    marginVertical: 50,
+  },
+
+  productContainer: {
+    width: '46%',
+    height: 350,
+    borderRadius: 20,
+    borderColor: '#000',
+    borderWidth: 1,
+    position: 'relative',
+    marginBottom: 20,
+  },
+
+  productMatchContainer: {
+    position: 'absolute',
+    top: -15,
+    alignSelf: 'center',
+    backgroundColor: '#000',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    zIndex: 2,
+    borderRadius: 20,
+    flexDirection: 'row',
+  },
+
+  productMatchText: {
+    color: '#fff',
+    fontSize: 12,
+  },
+
+  matchIcon: {
+    width: 15,
+    height: 15,
+    marginRight: 2,
+  },
+
+  productTopContainer: {
+    flex: 1,
+    backgroundColor: '#E9E9E9',
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  productImage: {
+    width: '60%',
+    height: '60%',
+    resizeMode: 'contain',
+  },
+
+  productBottomContainer: {
+    flex: 1,
+    padding: 10,
+    boxSizing: 'border-box',
+  },
+
+  productBottomTextContainer: {
+    gap: 5,
+    width: '90%',
+  },
+
+  name: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+
+  description: {
+    fontSize: 12,
+    color: '#000',
+  },
+
+  productBottomButtonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 10,
+    position: 'absolute',
+    bottom: 10,
+    alignSelf: 'center',
+    width: '100%',
+  },
+
+  productBottomButton: {
+    borderColor: '#000',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 20,
+  },
+
+  productBottomOptionsButton: {
+    borderColor: '#555',
+    borderWidth: 1,
+    borderRadius: 50,
+    height: 25,
+    width: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  productBottomButtonText: {
+    fontSize: 12,
+    color: '#000',
+  },
+
+  heart: {
+    backgroundColor: '#E9E9E9',
   },
 })
 
